539452c745130c3b1953e74587bf6b9d4c53a838
David Henriksson <davhen@kth.se>
2020-07-30 20:25:18 +0200

    Wiki queries done

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js')
 const axios = require("axios").default
 const baseUrl = "https://en.wikipedia.org/w/api.php"
 
@@ -5,7 +6,7 @@ module.exports = args => {
     const query = args.join(" ")
     if (args.length < 1) return
 
-    axios.get(baseUrl, {
+    return axios.get(baseUrl, {
         params: {
             action: "query",
             format: "json",
@@ -13,20 +14,60 @@ module.exports = args => {
             srsearch: `${args}`
         }
     })
-    .then(response => response.data.query.search)
-    .then(hits => axios.get(baseUrl, {
-        params: {
-                action: "query",
-                format: "json",
-                prop: "extracts",
-                titles: hits[0],
-                formatversion: "2",
-                exlimit: "1",
-                explaintext: "1",
-                exsectionformat: "wiki"
-        }
-    }))
-    .then(response => response.data.query.pages[0])
-    .then(page => console.log(page.extract))
+    .then(response => {
+        const hits = response.data.query.search
+        if (hits.length > 0) return handleSearchHits(hits)
+        else return handleNoSearchHits()
+    })
     .catch(error => console.error(error))
+}
+
+handleSearchHits = hits => {
+    return axios.get(baseUrl, {
+            params: {
+                    action: "query",
+                    format: "json",
+                    prop: "extracts|categories|pageimages|info|imageinfo",
+                    titles: hits[0].title,
+                    formatversion: "2",
+                    redirects: "1",
+                    exchars: "500",
+                    exlimit: "1",
+                    explaintext: "1",
+                    exsectionformat: "wiki",
+                    clcategories: "Category:All disambiguation pages",
+                    inprop: "url",
+                    pithumbsize: "500"
+            }
+        })
+    .then(response => response.data.query.pages[0])
+    .then(page => {
+        if (page.categories && page.categories.length > 0) {
+            // TODO: Do something different for disambiguations?
+        }
+        console.log(page)
+        return page ? embedWiki(page) : null
+    })
+    .catch(error => {
+        console.error(error)
+    })
+}
+
+handleNoSearchHits = () => {
+    let embed = new Discord.MessageEmbed()
+        .setTitle("No results")
+        .setDescription("Sorry, I couldn't find that for you.")
+        .setFooter("<:sad:710485918196432928>")
+    return embed
+}
+
+embedWiki = page => {
+    let embed = new Discord.MessageEmbed()
+        .setColor('#fa98cc')
+        .setTitle(`Wikipedia - ${page.title}`)
+        .setDescription(page.extract)
+        .setURL(page.fullurl)
+    if (page.thumbnail) embed.setImage(page.thumbnail.source)
+    embed.setThumbnail('https://safebooru.donmai.us/data/__w_arknights_and_1_more_drawn_by_pawamo_power__26d4b26107abdc75435582c02a0b4dde.gif')
+    return embed
 }
--- events/message.js
+++ events/message.js
@@ -2,17 +2,27 @@ const wiki = require("../commands/wiki.js")
 const doi = require("../commands/doi.js")
 
 module.exports = (client, msg) => {
-
     // ignore bots
     if(msg.author.bot) return;
     // ignore prefixes that aren't sensei's
     if(msg.content.indexOf(process.env.PREFIX) !== 0) return;
 
+    const channel = msg.channel
     const args = msg.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
+    const errorMsg = "Oops, something went wrong"
 
+    // Let commands do their thing and then return
+    // a promise that we can just post back to
+    // the channel on resolve
+    let responsePromise = null;
     switch(cmd) {
-        case 'w': wiki(args); break;
-        case 'doi': doi(args); break;
+        case 'w': responsePromise = wiki(args); break;
+        case 'doi': responsePromise = doi(args); break;
+        default: responsePromise = noCmd(); break;
     }
+
+    responsePromise.then(embed => channel.send(embed))
 }
+
+noCmd = () => new Promise().then(() => "No valid command received, what is a poor bot to do?")
